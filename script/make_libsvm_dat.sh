15805e7904f53025b4f08954913e4cd3bead7b36
lanjinsong <lanjinsong@s22.adsys.zwt.qihoo.net>
2015-05-04 15:01:59 +0800

    sort feature ids

--- script/make_libsvm_dat.sh
+++ script/make_libsvm_dat.sh
@@ -8,10 +8,15 @@ awk -F'[\t]' 'BEGIN{
       dict[$1] = $2
   }else{
       content=$1;
+      delete b;
       for(i=2;i<=NF;i++){
           if($i in dict){
-              content=content" "dict[$i]":1";
+              b[i] = dict[$i];
           }
+      }
+      n = asort(b);
+      for(i=1; i<=n; ++i){
+         content=content" "b[i]":1";
       }
       print content;
   }
